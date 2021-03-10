2f1e37d3fde5a088e74e0652c3bd5a4ac1fe61f5
ozaratem <zaratemorera.of@gmail.com>
2021-03-10 01:48:29 -0600

    Add Create, Read, Update and Delete

---
+++ public/main.js
@@ -0,0 +1,40 @@
+// main.js
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ => {
+  // Send PUT Request here
+  fetch('/quotes', {
+    method: 'put',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+        name: 'Darth Vadar',
+        quotes: 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(res => {
+    if (res.ok) return res.json()
+  })
+  .then(response => {
+    window.location.reload(true)
+  })
+})
+
+deleteButton.addEventListener('click', _ => {
+    fetch('/quotes', {
+      method: 'delete',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: 'Darth Vadar'
+    })
+  })
+    .then(response => {if (response === 'No quote to delete') {
+        messageDiv.textContent = 'No Darth Vadar quote to delete'
+      } else {
+        window.location.reload(true)
+      }
+  })
+  .cath(error => console.error(error))
+})
+
--- server.js
+++ server.js
@@ -7,28 +7,82 @@ const bodyParser= require('body-parser');
 const app = express();
 
 
-app.use(bodyParser.urlencoded({ extended: true }))
-
 app.listen(3000, function() {
-    console.log('listening on 3000')
-  })
+  console.log('listening on 3000')
+})
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html')
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('public'))
+app.use(bodyParser.json())
+
+//app.get(endpoint, callback)
+
+//app.get('/', (req, res) => {
+    //res.sendFile(__dirname + '/index.html')
      //Note: __dirname is the current directory you're in.
-    })
+    //})
 
-app.post('/quotes', (req, res) => {
-    console.log(req.body)
+//app.post('/quotes', (req, res) => {
+  //console.log(req.body)
+//})    
+
+MongoClient.connect("mongodb+srv://ozarate:a2d1bf7211@cluster0.a1jwj.mongodb.net/gym-logs?retryWrites=true&w=majority",
+ { useUnifiedTopology: true })
+.then(client => {
+  console.log('Connected to Database')
+  const db = client.db('gym-logs')
+  const quotesCollection = db.collection('quotes')
+
+  app.set('view engine', 'ejs')
+
+  app.post('/quotes', (req, res) => {
+    quotesCollection.insertOne(req.body)
+      .then(result => {
+        res.redirect('/')
+      })
+      .catch(error => console.error(error))
+  })
+  
+  app.get('/', (req, res) => {
+    db.collection('quotes').find().toArray()
+      .then(results => {
+        res.render('index.ejs', { quotes: results })
+      })
+      .catch(error => console.error(error))
+  })
+  
+  app.put('/quotes', (req, res) => {
+    quotesCollection.findOneAndUpdate(
+      { name: 'Yoda' },
+      {
+        $set: {
+          name: req.body.name,
+          quotes: req.body.quotes
+        }
+      },
+      {
+        upsert: true
+      })
+      .then(result => {res.json('Success')})
+      .catch(error => console.error(error))
+    
   })
 
-  MongoClient.connect("mongodb+srv://ozarate:a2d1bf7211@cluster0.a1jwj.mongodb.net/test?retryWrites=true&w=majority", { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('gym-server')
-    //app.use(/* ... */)
-    //app.get(/* ... */)
-    //app.post(/* ... */)
-    //app.listen(/* ... */)
+  app.delete('/quotes', (req, res) => {
+    // Handle delete event here
+    quotesCollection.deleteOne(
+      { name: req.body.name }
+    )
+      .then(result => {
+        if (result.deletedCount === 0) {
+          return res.json('No quote to delete')
+        }
+        res.json(`Deleted Darth Vadar's quote`)})
+      .catch(error => console.error(error))
   })
-  .catch(error => console.error(error))
+
+})
+
+.catch(error => console.error(error))
+
+
